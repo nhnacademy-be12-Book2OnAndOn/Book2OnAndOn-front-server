f4fdd2fc0bee89bae8e0929688959ee7be7a2a24
onyune <wjsdbwls0303@gmail.com>
2025-12-22 16:14:06 +0900

    검색

--- src/main/java/com/nhnacademy/book2onandonfrontservice/client/BookClient.java
+++ src/main/java/com/nhnacademy/book2onandonfrontservice/client/BookClient.java
@@ -20,6 +20,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -123,7 +124,7 @@ public interface BookClient {
     /// --------------- elastic search ----------------- 북 검색엔진
     @PostMapping("/api/books/search")
     Page<BookDto> searchBooks(@RequestHeader(value = "Authorization", required = false) String accessToken,
-                              @SpringQueryMap BookSearchCondition condition,//필드들을 뜯어서 검색조건으로 만듦 즉, 쿼리 파라미터로 만들수 있음
+                              @ModelAttribute BookSearchCondition condition,//필드들을 뜯어서 검색조건으로 만듦 즉, 쿼리 파라미터로 만들수 있음
                               @SpringQueryMap Pageable pageable);
 
 
