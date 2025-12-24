dbf63611498df06065c99cd0eaceae8611028248
OiJs <oijs9663@gmail.com>
2025-12-24 15:06:35 +0900

    Feature: 비회원 주문 토큰 발급, 비회원 주문 로그인 페이지 수정

--- src/main/java/com/nhnacademy/book2onandonfrontservice/dto/orderDto/GuestLoginRequestDto.java
+++ src/main/java/com/nhnacademy/book2onandonfrontservice/dto/orderDto/GuestLoginRequestDto.java
@@ -2,9 +2,11 @@ package com.nhnacademy.book2onandonfrontservice.dto.orderDto;// [Front Server] c
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class GuestLoginRequestDto {
     private String orderNumber;
     private String guestPassword;
